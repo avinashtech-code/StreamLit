d4fa5d84c1dfecd7ab9ead2d6812bff04f2ae2fb
avinashtech-code <avinash.tech149@gmail.com>
2026-07-04 00:16:02 +0530

    Update widgets.py

--- widgets.py
+++ widgets.py
@@ -43,3 +43,47 @@ age = st.slider(
     value=21
 )
 st.write("Age:", age)
+
+
+
+
+from datetime import date, time
+
+st.title("Date, Time & Color Picker Demo")
+
+# ===================================================
+# st.date_input()
+# ===================================================
+
+st.header("st.date_input()")
+
+# 1. Simple Date Input
+selected_date = st.date_input("Select Date")
+st.write("Selected Date:", selected_date)
+
+# ===================================================
+# st.time_input()
+# ===================================================
+
+st.header("st.time_input()")
+
+# 1. Simple Time Input
+selected_time = st.time_input("Select Time")
+st.write("Selected Time:", selected_time)
+
+
+# -----------------------------
+# 3. Color Picker
+# -----------------------------
+selected_color = st.color_picker(
+    "Choose Your Favorite Color",
+    value="#00FF00"
+)
+
+st.write("Selected Color:", selected_color)
+
+# Change text color using the selected color
+st.markdown(
+    f"<h2 style='color:{selected_color};'>Hello Avinash </h2>",
+    unsafe_allow_html=True
+)
